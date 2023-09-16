6d387a86f9b17bb280c9d2469617043e9508670f
AlexForOf <aleshmindr@gmail.com>
2023-09-17 01:49:57 +0200

    Finished work with Future tab, added functionality of browsing hours of each day separately in the future

--- src/components/main-content/Future.jsx
+++ src/components/main-content/Future.jsx
@@ -1,25 +1,57 @@
 import React from "react";
 import './Future.css';
 import DailyBlock from "./unified-comp/DailyBlock";
+import HourlyDailyBlock from "./unified-comp/HourlyBlock";
 
 function Future(props) {
     const {forecastday} = props.info.forecast
-    console.log(forecastday)
+
+    const [dailyBlocks, changeDailyBlocks] = React.useState([])
+    const [isReturned, switchIsReturned] = React.useState(true)
 
     function showHours(event, info) {
-        console.log(info)
+        const {hour} = info;
+        
+        const createdBlocks = hour.map((oneHour, index) => {
+            return <HourlyDailyBlock
+                    key={index}
+                    info={oneHour} 
+                    isImperial={props.isImperial}
+                    />
+        })
+
+        changeDailyBlocks(createdBlocks)
+        switchIsReturned(false)
     }
 
-    const dailyBlocks = forecastday.map((oneDay, index) => {
-        return <DailyBlock 
-            key={index}
-            info={oneDay}
-            isImperial={props.isImperial} 
-            showHours={showHours}
-            />
-    })
+    function showDays() {
+        const dailyBlocksDOM = forecastday.map((oneDay, index) => {
+            return <DailyBlock 
+                key={index}
+                info={oneDay}
+                isImperial={props.isImperial} 
+                showHours={showHours}
+                />
+        })
+        changeDailyBlocks(dailyBlocksDOM)
+        switchIsReturned(true)
+    }
+
+
+    React.useEffect(() => {
+        showDays()
+    }, [])
+
+    
+
+
     return (
         <div className="container-future">
+            {!isReturned && 
+                <div className="future-return">
+                    <h3 className="top-right-gotodetails return-arrow" onClick={showDays}>⮕</h3>
+                </div>
+            }
             {dailyBlocks}
         </div>
     )   
